b2febe23edc03c9b5efeb61631b1baf0ee0c6385
Olga Lyashevska <olga@herenstraat.nl>
2023-01-06 14:46:38 +0100

    fix: remove working files

--- lyshevska2016b-dev/r-code/fig/mytrim.sh
+++
@@ -1,2 +0,0 @@
-#to trim whitespace around the figure
-for i in *.png ; do convert $i -trim $i ; done
--- lyshevska2016b-dev/r-code/myfun/myfun.sample.R
+++
@@ -1,6 +0,0 @@
-myfun.sample <- function(x, ...){
-        if(all(is.na(x))){
-	            return(NA)
-    }
-    return(sample(x[!is.na(x)], ...))
-}
